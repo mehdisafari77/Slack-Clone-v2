33392bf585d512268ee7aed71b77c1d3fefaac18
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-11-22 20:19:59 -0800

    Slack Clone App

---
+++ src/components/Header.js
@@ -0,0 +1,88 @@
+import React from "react";
+import styled from 'styled-components'
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import { Avatar } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+
+function Header() {
+    return (
+        <HeaderContaner>
+
+            <HeaderLeft>
+                <HeaderAvatar
+                    // TODO: Add onClick
+                    
+                />
+                <AccessTimeIcon />
+            </HeaderLeft>
+
+            <HeaderSearch>
+                <SearchIcon />
+                <input placeholder="Search..." />
+            </HeaderSearch>
+
+            <HeaderRight>
+                <HelpOutlineIcon />
+            </HeaderRight>
+        </HeaderContaner>
+    )
+}
+
+export default Header;
+
+const HeaderContaner = styled.div`
+    display: flex;
+    position: fixed;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    background-color: var(--slack-color);
+    padding: 10px 0;
+    color: white;
+`;
+
+const HeaderLeft = styled.div`
+    flex: 0.3;
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+
+    > .MuiSvgIcon-root {
+        margin-left: auto;
+        margin-right: 30px;
+    }
+`;
+
+const HeaderSearch = styled.div`
+    display: flex;
+    flex: 0.4;
+    opacity: 1;
+    border-radius: 6px;
+    background-color: #421f44;
+    text-align: center;
+    padding: 0 50px;
+    color: gray;
+    border: 1px gray solid;
+
+    > input {
+        background-color: transparent;
+        border: none;
+        text-align: center;
+        min-width: 30vw;
+        outline: 0;
+        color: white;
+    }
+`;
+
+const HeaderRight = styled.div``;
+
+
+const HeaderAvatar = styled(Avatar)`
+    cursor: pointer;
+
+    :hover {
+        opacity: 0.8;
+    }
+`;
+
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDtY6cOPdQH_GdoKU9_PBTzX9de3Gli9OQ",
+    authDomain: "slack-clone-v2-4e0e6.firebaseapp.com",
+    projectId: "slack-clone-v2-4e0e6",
+    storageBucket: "slack-clone-v2-4e0e6.appspot.com",
+    messagingSenderId: "49297428724",
+    appId: "1:49297428724:web:571cd23e3128e9dafd4ea1"
+  };
