068fbd64112b193d496e2856ffa92bb1cceda7a5
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-11-23 12:22:33 -0800

    create the redux state management and test it in first room, app functional

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -45,72 +45,66 @@ function Sidebar() {
             <hr />
             <SidebarOption Icon={ExpandMoreIcon} title='Channels' />
             <hr />
-            <SidebarOption Icon={AddIcon}  addChanelOption title='Add Channels' />
-            {channels?.docs.map((doc) => (
-                <SidebarOption 
-                key={doc.id} 
-                title={doc.data().name} 
-                id={doc.id} />
-            ))}
-
-        </SidebarContainer>
-    )
+            <SidebarOption Icon={AddIcon} addChannelOption title="Add Channel" />
+      {channels?.docs.map((doc) => (
+        <SidebarOption key={doc.id} title={doc.data().name} id={doc.id} />
+      ))}
+    </SidebarContainer>
+  );
 }
 
-export default Sidebar
-
+export default Sidebar;
 
 const SidebarContainer = styled.div`
-    background-color: var(--slack-color);
-    color: white;
-    flex: 0.3;
-    margin-top: 60px;
-    max-width: 260px;
+  color: white;
+  background-color: var(--slack-color);
+  flex: 0.3;
+  border-top: 1px solid #49274b;
+  max-width: 260px;
+  margin-top: 60px;
 
-    > hr {
-        margin-top: 10px;
-        margin-bottom: 10px;
-        border: 1px solid #49274b
-    }
+  > hr {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #49274b;
+  }
 `;
 
 const SidebarHeader = styled.div`
-    display: flex;
-    border-bottom: 1px solid #49274b;
-    padding-bottom: 10px;
-    padding: 13px;
+  display: flex;
+  border-bottom: 1px solid #49274b;
+  padding-bottom: 10px;
+  padding: 13px;
 
-    > .MuiSvgIcon-root {
-        padding: 8px;
-        color: #49274b;
-        font-size: 18px;
-        background-color: white;
-        border-radius: 999px;
-    }
+  > .MuiSvgIcon-root {
+    padding: 8px;
+    color: #49274b;
+    font-size: 18px;
+    background-color: white;
+    border-radius: 999px;
+  }
 `;
 
 const SidebarInfo = styled.div`
-    flex: 1;
+  flex: 1;
 
-    > h2 {
-        font-size: 15px;
-        font-weight: 900;
-        margin-bottom: 5px;
-    }
+  > h2 {
+    font-size: 15px;
+    font-weight: 900;
+    margin-bottom: 5px;
+  }
 
-    > h3 {
-        display: flex;
-        font-size: 13px;
-        font-weight: 400;
-        align-items: center;
+  > h3 {
+    display: flex;
+    font-size: 13px;
+    font-weight: 400;
+    align-items: center;
+  }
 
-    }
-
-    > h3 > .MuiSvgIcon-root {
-        font-size: 14px;
-        margin-top: 1px;
-        margin-right: 2px;
-        color: green;
-    }
+  > h3 > .MuiSvgIcon-root {
+    font-size: 14px;
+    margin-top: 1px;
+    margin-right: 2px;
+    color: green;
+  }
 `;
-
