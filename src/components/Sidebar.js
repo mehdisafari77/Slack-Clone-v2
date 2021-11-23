3d82f28cd38ce1884dfccaa79e194df770413cf5
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-11-22 23:31:19 -0800

    add styled components icons in sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,16 @@
-import React from 'react'
-import styled from 'styled-components'
-import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import CreateIcon from '@mui/icons-material/Create';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import React from 'react';
+import styled from 'styled-components';
+import SidebarOption from './SidebarOption';
+import InsertCommentIcon from '@mui/icons-material/InsertComment';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import AppsIcon from '@mui/icons-material/Apps';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 
 function Sidebar() {
     return (
@@ -16,6 +25,16 @@ function Sidebar() {
                 </SidebarInfo>
                 <CreateIcon />
             </SidebarHeader>
+
+            <SidebarOption Icon={InsertCommentIcon} title='Threads' />
+            <SidebarOption Icon={InboxIcon} title='Mentions & reactions' />
+            <SidebarOption Icon={DraftsIcon} title='Saved items' />
+            <SidebarOption Icon={BookmarkBorderIcon} title='Channel browser' />
+            <SidebarOption Icon={PeopleAltIcon} title='People & user groups' />
+            <SidebarOption Icon={AppsIcon} title='Apps' />
+            <SidebarOption Icon={FileCopyIcon} title='File browser' />
+            <SidebarOption Icon={ExpandLessIcon} title='Show less' />
+
         </SidebarContainer>
     )
 }
---
+++ src/components/SidebarOption.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+function SidebarOption(props) {
+    return (
+        <SidebarOptionContainer></SidebarOptionContainer>
+    )
+}
+
+export default SidebarOption
+
+const SidebarOptionContainer = styled.div``;
+
