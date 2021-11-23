8357d47ec576a33cd188376c8a4c3bd7bb844a2c
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-11-22 23:19:05 -0800

    initial sidebar header stylig done

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import CreateIcon from '@mui/icons-material/Create';
 
 function Sidebar() {
     return (
@@ -13,6 +14,7 @@ function Sidebar() {
                         Mehdi Safari
                     </h3>
                 </SidebarInfo>
+                <CreateIcon />
             </SidebarHeader>
         </SidebarContainer>
     )
@@ -22,13 +24,50 @@ export default Sidebar
 
 
 const SidebarContainer = styled.div`
-
+    background-color: var(--slack-color);
+    color: white;
+    flex: 0.3;
+    margin-top: 60px;
+    max-width: 260px;
 `;
 
 const SidebarHeader = styled.div`
+    display: flex;
+    border-bottom: 1px solid #49274b;
+    padding-bottom: 10px;
+    padding: 13px;
 
+    > .MuiSvgIcon-root {
+        padding: 8px;
+        color: #49274b;
+        font-size: 18px;
+        background-color: white;
+        border-radius: 999px;
+    }
 `;
 
 const SidebarInfo = styled.div`
+    flex: 1;
 
+    > h2 {
+        font-size: 15px;
+        font-weight: 900;
+        margin-bottom: 5px;
+    }
+
+    > h3 {
+        display: flex;
+        font-size: 13px;
+        font-weight: 400;
+        align-items: center;
+
+    }
+
+    > h3 > .MuiSvgIcon-root {
+        font-size: 14px;
+        margin-top: 1px;
+        margin-right: 2px;
+        color: green;
+    }
 `;
+
