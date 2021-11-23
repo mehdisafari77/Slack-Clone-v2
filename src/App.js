8bd4bc932aa126d930898983ef92a1ccc8c62dc1
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-11-23 12:46:15 -0800

    add initial header part of chat section, app functional

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 import Header from './components/Header';
 import styled from 'styled-components'
 import Sidebar from './components/Sidebar';
+import Chat from "./components/Chat";
 
 
 function App() {
